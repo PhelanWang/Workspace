62897dc3ba3f1c16c7ce07ad480cb898cdacb240
PhelanWang <1825633959@qq.com>
2018-01-25 14:05:33 +0800

    modify test

--- NaiveBayesianClassification.py
+++ NaiveBayesianClassification.py
@@ -14,8 +14,11 @@ def normal_distribution(x, mean, var):
 
 data = np.loadtxt('./data/logistic_data.txt', delimiter=',')
 
-X = data[:, 0:2]
-Y = data[:, 2:3]
+X = data[:70, 0:2]
+Y = data[:70, 2:3]
+
+TestX = data[70:, 0:2]
+TestY = data[70:, 2:3]
 
 # 40 20的概率
 t0 = 40
@@ -48,15 +51,16 @@ print '将样本(%.2f, %.2f)分类到%d.' % (t0, t1, result1 > result0)
 
 
 # 用得到的模型估计已有的分类样本
-results = np.zeros((100, 1), dtype=np.int)
-for i in range(0, Y.size):
-    X0_Y0 = normal_distribution(X[i, 0], X_Y0[:, 0].mean(), X_Y0[:, 0].var())
-    X1_Y0 = normal_distribution(X[i, 1], X_Y0[:, 1].mean(), X_Y0[:, 1].var())
+results = np.zeros((30, 1), dtype=np.int)
+
+for i in range(0, TestY.size):
+    X0_Y0 = normal_distribution(TestX[i, 0], X_Y0[:, 0].mean(), X_Y0[:, 0].var())
+    X1_Y0 = normal_distribution(TestX[i, 1], X_Y0[:, 1].mean(), X_Y0[:, 1].var())
     result0 = P_Y0 * (X0_Y0 + X1_Y0)
-    X0_Y1 = normal_distribution(X[i, 0], X_Y1[:, 0].mean(), X_Y1[:, 0].var())
-    X1_Y1 = normal_distribution(X[i, 0], X_Y1[:, 1].mean(), X_Y1[:, 1].var())
+    X0_Y1 = normal_distribution(TestX[i, 0], X_Y1[:, 0].mean(), X_Y1[:, 0].var())
+    X1_Y1 = normal_distribution(TestX[i, 0], X_Y1[:, 1].mean(), X_Y1[:, 1].var())
     result1 = P_Y1 * (X0_Y1 + X1_Y1)
     results[i] = result1 > result0
 
 # 计算用得到的模型得出的分类的正确率
-print '用得到的模型得出的分类正确率为: %.2f%%' % (float(sum(results == Y))/Y.size)
+print '用得到的模型得出的分类正确率为: %.2f%%' % (float(sum(results == TestY)*100)/TestY.size)
